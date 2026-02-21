144dce8efce70b0390252b8356e839bf1bbff1bf
Charmi Padh <charmi.padh030206@gmail.com>
2026-02-21 13:13:44 +0530

    page 6 backend

---
+++ page-6-trip-expense-fuel/backend/app/models/__init__.py
@@ -0,0 +1,5 @@
+from app.models.enums import * # type: ignore
+from app.models.vehicle import * # type: ignore
+from app.models.trip import * # type: ignore
+from app.models.maintenance import * # type: ignore
+from app.models.expense import * # type: ignore
